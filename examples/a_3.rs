62d7648eb0a77c45d05f192a74918e12e932f6ed
Urhengulas <johann.hemmann@code.berlin>
2022-09-06 15:47:53 +0200

    a3

---
+++ examples/a_3.rs
@@ -0,0 +1,37 @@
+#![feature(allocator_api)]
+
+use core::slice;
+use std::{
+    alloc::{AllocError, Allocator, Layout, System},
+    ptr::{self, NonNull},
+};
+
+static MY_ALLOC: MyAllocator = MyAllocator;
+
+#[derive(Clone, Copy)]
+struct MyAllocator;
+
+unsafe impl Allocator for MyAllocator {
+    fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+        unsafe {
+            let ptr = slice::from_raw_parts_mut(ptr::null_mut(), layout.size()) as *mut _;
+            let non_null = NonNull::new_unchecked(ptr);
+            println!("Allocated!");
+            Ok(non_null)
+        }
+    }
+
+    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+        System.deallocate(ptr, layout)
+    }
+}
+
+fn main() {
+    let mut a = Vec::<i32, _>::new_in(MY_ALLOC);
+    dbg!(&a, a.as_ptr());
+
+    for i in 0..5 {
+        a.push(i);
+    }
+    dbg!(&a, a.as_ptr());
+}
