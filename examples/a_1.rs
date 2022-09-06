0aff70da6dd51d176dc3c0e71127613d52cc0332
Urhengulas <johann.hemmann@code.berlin>
2022-09-06 15:04:41 +0200

    a1

---
+++ examples/a_1.rs
@@ -0,0 +1,32 @@
+#![feature(allocator_api)]
+
+use std::{
+    alloc::{AllocError, Allocator, Layout, System},
+    ptr::NonNull,
+};
+
+static MY_ALLOC: MyAllocator = MyAllocator;
+
+#[derive(Clone, Copy)]
+struct MyAllocator;
+
+unsafe impl Allocator for MyAllocator {
+    fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+        System.allocate(layout)
+    }
+
+    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+        System.deallocate(ptr, layout)
+    }
+}
+
+fn main() {
+    let mut a = Vec::with_capacity_in(3, MY_ALLOC);
+    (0..3).for_each(|i| a.push(i));
+
+    let mut b = Vec::with_capacity_in(3, MY_ALLOC);
+    (0..3).for_each(|i| b.push(i * 2));
+
+    dbg!(&a, a.as_ptr());
+    dbg!(&b, b.as_ptr());
+}
