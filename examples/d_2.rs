f99fcc20d0bb2fb787ec487488833e5a68a4e89c
Urhengulas <johann.hemmann@code.berlin>
2022-09-12 11:30:55 +0200

    update d2

--- examples/d_2.rs
+++ examples/d_2.rs
@@ -9,12 +9,16 @@ fn main() {
 }
 
 fn push_one_mb(a: &mut Vec<[u32; KB]>) {
-    if a.try_reserve_exact(1024 * KB).is_ok() {
-        for _ in 0..(1024) {
-            a.push([0; KB]);
+    match a.try_reserve_exact(1024 * KB) {
+        Ok(_) => {
+            for _ in 0..(1024) {
+                a.push([0; KB]);
+            }
         }
-    } else {
-        println!("Heap memory full. Handle it gracefully.");
-        std::process::exit(0);
+        Err(e) => {
+            dbg!(e);
+            println!("Heap memory full. Handle it gracefully.");
+            std::process::exit(0);
+        }
     }
 }
