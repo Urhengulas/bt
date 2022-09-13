b9e8a4580059e167348836cfcdbd549d413fed23
Urhengulas <johann.hemmann@code.berlin>
2022-09-13 15:25:02 +0200

    update e2

--- examples/e_2.rs
+++ examples/e_2.rs
@@ -8,42 +8,45 @@ use std::{
 
 fn main() {
     // allocate a Vec
-    let mut a = vec![1, 2, 3, 4, 5];
-    println!("# allocate a\na: {:?}\n", &a);
+    println!("# allocate a");
+    let mut a = (0..3).into_iter().map(|i| Dropper(i)).collect::<Vec<_>>();
+    println!("a: {:?}\n", &a);
 
     // deallocate memory
-    let ptr = a.as_mut_ptr();
-    let layout = Layout::array::<u8>(a.capacity()).unwrap();
+    println!("# deallocate a");
+    let ptr = a.as_mut_ptr().cast();
+    let layout = Layout::array::<Dropper>(a.capacity()).unwrap();
     unsafe {
         System.deallocate(NonNull::new(ptr).unwrap(), layout);
     }
 
     // access memory after deallocation; gives garbage values
-    println!("# deallocate a\na: {:?}\n", &a);
+    println!("a: {:?}\n", &a);
 
-    // allocate a String; this will have the same memory address as the Vec had
+    // allocate a String; this will use the same memory address as a
+    println!("# allocate b");
     let mut b = "Hello, world!".to_string();
-    assert_eq!(b.as_mut_ptr(), ptr);
-    println!(
-        "# allocate b\na: {:?},\nb: {:?} ({:?})\n",
-        &a,
-        &b.as_bytes(),
-        &b,
-    );
+    assert_eq!(ptr, b.as_mut_ptr());
+    println!("b: {:?} ({:?})\na: {:?}\n", &b.as_bytes(), &b, &a);
 
+    println!("# write to a");
     // write to the old vec; this will also write into the new String
     for (idx, val) in a.iter_mut().enumerate() {
-        *val = 2 * (idx as u8);
+        val.0 = 2 * idx as u16;
     }
 
     // scary!
-    println!(
-        "# write to a\na: {:?},\nb: {:?} ({:?})",
-        &a,
-        &b.as_bytes(),
-        &b
-    );
+    println!("a: {:?},\nb: {:?} ({:?})", &a, &b.as_bytes(), &b);
 
     // forget about memory to avoid double free error
-    mem::forget(a);
+    mem::forget(b);
+}
+
+#[derive(Debug)]
+struct Dropper(u16);
+
+impl Drop for Dropper {
+    fn drop(&mut self) {
+        println!("Dropping {}", self.0);
+    }
 }
