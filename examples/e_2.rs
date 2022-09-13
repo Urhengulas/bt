3f698b5d5398b5b07cc3e0af937c3a52c6ac0c32
Urhengulas <johann.hemmann@code.berlin>
2022-09-13 12:39:48 +0200

    fix e2

--- examples/e_2.rs
+++ examples/e_2.rs
@@ -9,21 +9,27 @@ use std::{
 fn main() {
     // allocate a Vec
     let mut a = vec![1, 2, 3, 4, 5];
-    dbg!(&a);
+    println!("# allocate a\na: {:?}\n", &a);
 
     // deallocate memory
     let ptr = a.as_mut_ptr();
+    let layout = Layout::array::<u8>(a.capacity()).unwrap();
     unsafe {
-        System.deallocate(NonNull::new(ptr).unwrap(), Layout::new::<Vec<u8>>());
+        System.deallocate(NonNull::new(ptr).unwrap(), layout);
     }
 
     // access memory after deallocation; gives garbage values
-    dbg!(&a);
+    println!("# deallocate a\na: {:?}\n", &a);
 
     // allocate a String; this will have the same memory address as the Vec had
     let mut b = "Hello, world!".to_string();
     assert_eq!(b.as_mut_ptr(), ptr);
-    dbg!(&a, &b.as_bytes());
+    println!(
+        "# allocate b\na: {:?},\nb: {:?} ({:?})\n",
+        &a,
+        &b.as_bytes(),
+        &b,
+    );
 
     // write to the old vec; this will also write into the new String
     for (idx, val) in a.iter_mut().enumerate() {
@@ -31,7 +37,12 @@ fn main() {
     }
 
     // scary!
-    dbg!(&a, &b.as_bytes(), &b);
+    println!(
+        "# write to a\na: {:?},\nb: {:?} ({:?})",
+        &a,
+        &b.as_bytes(),
+        &b
+    );
 
     // forget about memory to avoid double free error
     mem::forget(a);
