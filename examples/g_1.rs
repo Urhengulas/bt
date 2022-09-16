5fc66f893b4a00cfa287d053088012b7340c865d
Urhengulas <johann.hemmann@code.berlin>
2022-09-16 12:23:39 +0200

    update g1

--- examples/g_1.rs
+++ examples/g_1.rs
@@ -2,23 +2,27 @@
 
 use std::{
     alloc::{AllocError, Allocator, Layout, System},
+    fmt::Debug,
     ptr::NonNull,
     thread,
     time::Duration,
 };
 
 static MY_ALLOC: MyAllocator = MyAllocator;
+const DUR: Duration = Duration::from_secs(1);
 
 struct MyAllocator;
 
 unsafe impl Allocator for MyAllocator {
     fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        thread::sleep(Duration::from_secs(2));
+        thread::sleep(DUR);
         println!("Allocate!");
         System.allocate(layout)
     }
 
     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+        thread::sleep(DUR);
+        println!("Deallocate!");
         System.deallocate(ptr, layout)
     }
 }
@@ -28,17 +32,13 @@ struct SlowDrop(u32);
 
 impl Drop for SlowDrop {
     fn drop(&mut self) {
-        thread::sleep(Duration::from_secs(1));
+        thread::sleep(DUR);
         println!("Drop!");
     }
 }
 
 fn main() {
     let mut a = Vec::new_in(&MY_ALLOC);
-    (0..3).for_each(|i| a.push(SlowDrop(i)));
-    dbg!(&a, a.as_ptr());
-
-    let mut b = Vec::new_in(&MY_ALLOC);
-    (0..5).for_each(|i| b.push(SlowDrop(i)));
-    dbg!(&b, b.as_ptr());
+    (0..10).for_each(|i| a.push(SlowDrop(i)));
+    println!("{:?}, {:?}", &a, a.as_ptr());
 }
