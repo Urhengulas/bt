dc4fb99b041ca8e641e097c1d7ad93ca88bb088f
Urhengulas <johann.hemmann@code.berlin>
2022-10-25 16:02:14 +0200

    Rename c2 to c1

--- yes_std/src/bin/c1.rs
+++ yes_std/src/bin/c1.rs
@@ -1,22 +1,38 @@
 #![feature(allocator_api)]
 
-use yes_std::{BumpAllocator, Locked};
+use std::{
+    alloc::{Allocator, Global, Layout},
+    ptr::NonNull,
+};
 
-static mut MEMORY: [u8; 300] = [0; 300];
-static ALLOC: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+#[derive(Debug)]
+struct LeakyVec<T, A: Allocator> {
+    alloc: A,
+    cap: usize,
+    len: usize,
+    ptr: NonNull<T>,
+}
 
-fn main() {
-    init_heap();
-
-    let mut a = Vec::new_in(&ALLOC);
-    for i in 0.. {
-        a.push(i);
+impl<T, A: Allocator> LeakyVec<T, A> {
+    fn with_capacity_in(cap: usize, alloc: A) -> Self {
+        let ptr = alloc.allocate(Layout::array::<T>(cap).unwrap()).unwrap();
+        Self {
+            alloc,
+            cap,
+            len: 0,
+            ptr: ptr.cast(),
+        }
     }
 }
 
-fn init_heap() {
-    unsafe {
-        let mut allocator = ALLOC.lock();
-        allocator.init(MEMORY.as_ptr() as usize, MEMORY.len());
+impl<T, A: Allocator> Drop for LeakyVec<T, A> {
+    fn drop(&mut self) {
+        let layout = Layout::array::<T>(self.len).unwrap(); // bug: should use `self.cap` instead of `self.len`
+        unsafe { self.alloc.deallocate(self.ptr.cast(), layout) }
     }
+}
+
+fn main() {
+    let a = LeakyVec::<u32, _>::with_capacity_in(10, &Global);
+    dbg!(a);
 }
