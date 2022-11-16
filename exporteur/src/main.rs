ff44badf2e7b9da634e7ede027204b2fa5325ba5
Urhengulas <johann.hemmann@code.berlin>
2022-11-16 01:58:46 +0100

    Sort the files

--- exporteur/src/main.rs
+++ exporteur/src/main.rs
@@ -11,11 +11,12 @@ fn main() {
 }
 
 fn print_dir(dir: PathBuf) {
-    let files = dir
+    let mut files = dir
         .read_dir()
         .unwrap()
         .filter_map(|entry| entry.map(|e| e.path()).ok())
         .collect::<Vec<_>>();
+    files.sort_unstable();
     for f in files.into_iter() {
         let file_name = f.file_name().unwrap().to_string_lossy();
         let content = fs::read_to_string(&f).unwrap();
